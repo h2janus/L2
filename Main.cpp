39c33ab3f7686e47707fa7448a687cbc50d9f2be
Stranger484 <gladstranger@gmail.com>
2025-03-23 14:08:37 +0700

    Change ShellExecute to CteateProcess

--- Main.cpp
+++ Main.cpp
@@ -9,5 +9,29 @@
 // You can change the path to the .bin file to avoid including the L2.exe inside the System folder.
 // Author: Zoey76
 int _stdcall wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
-	ShellExecute(0, L"open", L"cmd.exe", L"/C start l2.bin IP=127.0.0.1", 0, SW_HIDE);
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+
+    CreateProcess(L"l2.bin",     // Module name
+        L"l2.bin IP=127.0.0.1",  // Command line
+        NULL,                    // Process handle not inheritable
+        NULL,                    // Thread handle not inheritable
+        FALSE,                   // Set handle inheritance to FALSE
+        0,                       // No creation flags
+        NULL,                    // Use parent's environment block
+        NULL,                    // Use parent's starting directory 
+        &si,                     // Pointer to STARTUPINFO structure
+        &pi );                   // Pointer to PROCESS_INFORMATION structure
+
+    // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
 }
